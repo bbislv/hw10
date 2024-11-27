6a6251493ea83d687965326a58d13698615bd935
ekaterinabobyleva <bbislv12@gmail.com>
2024-11-27 15:45:07 +0300

    hw10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        //task 1
+        System.out.println("task 1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        //task 2
+        System.out.println("task 2");
+
+        String upperCaseFullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + upperCaseFullName);
+
+        //task 3
+        System.out.println("task 3");
+
+        fullName = "Иванов Семён Семёнович";
+        String replacedFullName = fullName.replace('ё','е');
+        System.out.println("Данные ФИО сотрудника - " + replacedFullName);
     }
 }
+
